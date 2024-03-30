d615f6a5768f263fed63830f0ddaa0d2d027f87b
juanvrgr <jnmvrgr@gmail.com>
2024-03-30 02:37:39 -0300

    Changed years of experience

--- src/containers/Home/language.js
+++ src/containers/Home/language.js
@@ -1,9 +1,9 @@
 const language = {
   en: {
-    title: "Hi! I'm Juan",
+    title: "Hey there!",
     subtitle: "Software Developer",
     data: [
-      "I am a passionate Software Developer with +2.5 years of experience. I have experience building applications with JavaScript / TypeScript / Python / Java / React.js / Redux / React-Native / Node.js / Express / PostgreSQL / MySQL / Sequelize / MongoDB / Mongoose / AWS / Redis / HTML5 / CSS / SASS.",
+      "I'm Juan Vergara, a passionate Software Developer with 3 years of experience. I have experience building applications with JavaScript / TypeScript / Python / Java / React.js / Redux / React-Native / Node.js / Express / PostgreSQL / MySQL / Sequelize / MongoDB / Mongoose / AWS / Redis / HTML5 / CSS / SASS.",
       "I love good practices and I'm very curious when developing. I love tech which leads me to a proactive attitude learning every day, and wanting to improve my skills.",
       "I invite you to see my resume to see more details about my experience and my projects."
     ],
@@ -12,10 +12,10 @@ const language = {
   },
 
   es: {
-    title: "Hola! Soy Juan",
+    title: "Hola!",
     subtitle: "Software Developer",
     data: [
-      "Soy un apasionado Software Developer con +2.5 años de experiencia. Tengo experiencia creando aplicaciones con JavaScript / TypeScript / Python / Java / React.js / Redux / React-Native / Node.js / Express / PostgreSQL / MySQL / Sequelize / MongoDB / Mongoose / AWS / Redis / HTML5 / CSS / SASS.",
+      "Soy Juan Vergara, un apasionado Software Developer con 3 años de experiencia. Tengo experiencia creando aplicaciones con JavaScript / TypeScript / Python / Java / React.js / Redux / React-Native / Node.js / Express / PostgreSQL / MySQL / Sequelize / MongoDB / Mongoose / AWS / Redis / HTML5 / CSS / SASS.",
       "Me encantan las buenas prácticas y soy muy curioso al programar. Me encanta la tecnologia y eso me genera una actitud proactiva aprendiendo todos los dias, y mejorando mis habilidades",
       "Te invito a ver mi curriculum para ver más detalles sobre mi experiencia y mis proyectos."
     ],
